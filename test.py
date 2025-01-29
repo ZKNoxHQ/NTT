ea726d0bda06cc9fb1a33140436b3264999bafa3
Simon Masson <simon.masson@yahoo.fr>
2025-01-29 12:44:09 +0100

    tests works untill 2¹⁰

--- test.py
+++ test.py
@@ -81,7 +81,7 @@ def test(n, iterations=500):
 # Run all the tests
 if (__name__ == "__main__"):
 
-    for i in range(2, 8):  # 11):
+    for i in range(2, 11):
         n = (1 << i)
         it = 100
         print("Test battery for n = {n}".format(n=n))
