69cab245f460f73cc625c6a88a68573541a9f1a8
Simon Masson <simon.masson@yahoo.fr>
2025-02-05 12:34:36 +0100

    add simple benchmarks

---
+++ assets/pythonref/bench_poly_mul.py
@@ -0,0 +1,44 @@
+from mert import TestMert
+from random import randint
+from time import time
+from generate_test_vectors import deterministic_poly
+from poly import Poly
+from test_cases import TEST_CASES
+
+iterations = 100
+print("iterations:{}\n".format(iterations))
+
+print("        q\tn\tPatrick\t\tSchoolbook\tMert")
+for (q, two_adicity) in TEST_CASES:
+
+    for n in [1 << (two_adicity-2), 1 << (two_adicity-1)]:  # for two sizes of polynomials
+        print("{:10}\t{}".format(q, n), end='\t')
+        p1 = Poly(deterministic_poly(q, n), q)
+        p2 = Poly(deterministic_poly(q, n), q)
+        t1 = time()
+        for i in range(iterations):
+            p3 = p1*p2
+        t2 = time()
+        p3_patrick = p3
+        print("{:.2f} ms".format((t2-t1) * 10**3), end='\t\t')
+
+        p1 = Poly(deterministic_poly(q, n), q)
+        p2 = Poly(deterministic_poly(q, n), q)
+        t3 = time()
+        for i in range(iterations):
+            p3 = p1.mul_schoolbook(p2)
+        t4 = time()
+        p3_schoolbook = p3
+        print("{:.2f} ms".format((t4-t3) * 10**3), end='\t\t')
+
+        Mert = TestMert(q)
+        p1 = Poly(deterministic_poly(q, n), q)
+        p2 = Poly(deterministic_poly(q, n), q)
+        t5 = time()
+        for i in range(iterations):
+            p3 = Mert.poly_mul_mert(p1.coeffs, p2.coeffs)
+        t6 = time()
+        p3_mert = p3
+        print("{:.2f} ms".format((t6-t5) * 10**3))
+
+        assert p3_mert == p3_patrick.coeffs and p3_mert == p3_schoolbook.coeffs
--- assets/pythonref/mert.py
+++ assets/pythonref/mert.py
@@ -3,13 +3,16 @@ from ntt_constants import n_inv, ψ_rev, ψ_inv_rev, ψ as Ψ, ψ_inv as Ψ_inv
 from random import randint
 from poly import Poly
 from math import log
-import unittest
 from ntt import NTT
 
-q = 3329
+# q = 3329
 
 
-class TestMert(unittest.TestCase):
+class TestMert:
+
+    def __init__(self, q):
+        self.q = q
+
     def intReverse(self, a, n):
         b = ('{:0'+str(n)+'b}').format(a)
         return int(b[::-1], 2)
@@ -22,14 +25,14 @@ class TestMert(unittest.TestCase):
         for s in range(int(log(N, 2)), 0, -1):
             m = 2**s
             for k in range(int(N/m)):
-                # TW = pow(ω, self.intReverse(k, int(log(N, 2))-s)*int(m/2), q)
-                TW = ψ_rev[q][k]
+                # TW = pow(ω, self.intReverse(k, int(log(N, 2))-s)*int(m/2), self.q)
+                TW = ψ_rev[self.q][k]
                 for j in range(int(m/2)):
                     u = B[k*m+j]
-                    t = (TW*B[k*m+j+int(m/2)]) % q
+                    t = (TW*B[k*m+j+int(m/2)]) % self.q
 
-                    B[k*m+j] = (u+t) % q
-                    B[k*m+j+int(m/2)] = (u-t) % q
+                    B[k*m+j] = (u+t) % self.q
+                    B[k*m+j+int(m/2)] = (u-t) % self.q
 
         return B
 
@@ -51,13 +54,13 @@ class TestMert(unittest.TestCase):
                 jt = 0
                 while j < (N-1):
                     # bit-reversing jt
-                    # TW = pow(ω_inv, self.intReverse(jt, int(log(N >> 1, 2))), q)
-                    TW = ψ_inv_rev[q][jt]
+                    # TW = pow(ω_inv, self.intReverse(jt, int(log(N >> 1, 2))), self.q)
+                    TW = ψ_inv_rev[self.q][jt]
 
                     temp = B[j]
 
-                    B[j] = (temp + B[j+d]) % q
-                    B[j+d] = (temp - B[j+d])*TW % q
+                    B[j] = (temp + B[j+d]) % self.q
+                    B[j+d] = (temp - B[j+d])*TW % self.q
 
                     jt = jt+1
                     j = j + 2*m
@@ -65,7 +68,7 @@ class TestMert(unittest.TestCase):
             v = int(v/2)
             d = 2*d
 
-        return [(x*n_inv[q][N]) % q for x in B]
+        return [(x*n_inv[self.q][N]) % self.q for x in B]
 
     def mul_schoolbook_x_n_minus_one(self, A, B):
         """Multiplication of two polynomials using the schoolbook algorithm."""
@@ -77,63 +80,70 @@ class TestMert(unittest.TestCase):
         D = [0] * (n)
         for j, f_j in enumerate(f):
             for k, g_k in enumerate(g):
-                C[j+k] = (C[j+k] + f_j * g_k) % q
+                C[j+k] = (C[j+k] + f_j * g_k) % self.q
         # reduction modulo x^n  + 1
         for i in range(n):
-            D[i] = (C[i] + C[i+n]) % q
+            D[i] = (C[i] + C[i+n]) % self.q
         return D
 
     def prec(self, A):
         n = len(A)
         # list of roots for the precomputations
-        Ψ0 = Ψ[q][::len(Ψ[q])//n]
-        return [(x * y) % q for (x, y) in zip(A, Ψ0)]
+        Ψ0 = Ψ[self.q][::len(Ψ[self.q])//n]
+        return [(x * y) % self.q for (x, y) in zip(A, Ψ0)]
 
     def post(self, A):
         n = len(A)
         # list of roots for the precomputations
-        Ψ0_inv = Ψ_inv[q][::len(Ψ_inv[q])//n]
-        return [(x * y) % q for (x, y) in zip(A, Ψ0_inv)]
+        Ψ0_inv = Ψ_inv[self.q][::len(Ψ_inv[self.q])//n]
+        return [(x * y) % self.q for (x, y) in zip(A, Ψ0_inv)]
 
-    def test_mert(self):
+    def poly_mul_mert(self, A, B):
+        A_ntt = self.ntt_mert(self.prec(A))
+        B_ntt = self.ntt_mert(self.prec(B))
+        A_B_ntt = [(x*y) % self.q for (x, y) in zip(A_ntt, B_ntt)]
+        return self.post(self.intt_mert(A_B_ntt))
 
-        f = [1, 2, 3, 4]
-        # f = [randint(0, q-1) for i in range(4)]
-        F = self.ntt_mert(f)
-        back_f = self.intt_mert(F)
-        for i in range(len(f)):
-            assert back_f[i] == f[i]
-        g = [5, 6, 7, 8]
-        # g = [randint(0, q-1) for i in range(4)]
-        G = self.ntt_mert(g)
-        back_g = self.intt_mert(G)
-        for i in range(len(g)):
-            assert back_g[i] == g[i]
 
-        # check multiplication mod x^n-1
-        F_mul_G = [(x*y) % q for (x, y) in zip(F, G)]
-        f_mul_g = self.intt_mert(F_mul_G)
-        assert f_mul_g == self.mul_schoolbook_x_n_minus_one(f, g)
-        # check multiplication mod x^n+1
-        fp = self.prec(f)
-        gp = self.prec(g)
-        Fp = self.ntt_mert(fp)
-        Gp = self.ntt_mert(gp)
-        Fp_mul_Gp = [(x*y) % q for (x, y) in zip(Fp, Gp)]
-        fp_mul_gp = self.intt_mert(Fp_mul_Gp)
-        f_mul_g_2 = self.post(fp_mul_gp)
-        assert f_mul_g_2 == (Poly(f, q) * Poly(g, q)).coeffs
+q = 3329
+Mert = TestMert(q)
+f = [1, 2, 3, 4]
+# f = [randint(0, q-1) for i in range(4)]
+F = Mert.ntt_mert(f)
+back_f = Mert.intt_mert(F)
+for i in range(len(f)):
+    assert back_f[i] == f[i]
+g = [5, 6, 7, 8]
+# g = [randint(0, q-1) for i in range(4)]
+G = Mert.ntt_mert(g)
+back_g = Mert.intt_mert(G)
+for i in range(len(g)):
+    assert back_g[i] == g[i]
 
-        # now, the other way around, using ntt and intt instead of ntt_mert and intt_mert
-        T = NTT(q)
-        assert self.ntt_mert(self.prec(f)) == T.ntt(f)
-        assert self.post(self.intt_mert(F)) == T.intt(F)
-        assert self.ntt_mert(f) == T.ntt(self.post(f))
-        assert self.intt_mert(F) == self.prec(T.intt(F))
+# check multiplication mod x^n-1
+F_mul_G = [(x*y) % q for (x, y) in zip(F, G)]
+f_mul_g = Mert.intt_mert(F_mul_G)
+assert f_mul_g == Mert.mul_schoolbook_x_n_minus_one(f, g)
+# check multiplication mod x^n+1
+fp = Mert.prec(f)
+gp = Mert.prec(g)
+Fp = Mert.ntt_mert(fp)
+Gp = Mert.ntt_mert(gp)
+Fp_mul_Gp = [(x*y) % q for (x, y) in zip(Fp, Gp)]
+fp_mul_gp = Mert.intt_mert(Fp_mul_Gp)
+f_mul_g_2 = Mert.post(fp_mul_gp)
+assert f_mul_g_2 == (Poly(f, q) * Poly(g, q)).coeffs
 
-        # now we compute the multiplication mod x^n-1 using T.(i)ntt:
-        F1 = T.ntt(self.post(f))
-        G1 = T.ntt(self.post(g))
-        F1_mul_G1 = [(x*y) % q for (x, y) in zip(F1, G1)]
-        f1_mul_g1 = self.prec(T.intt(F1_mul_G1))
-        assert f1_mul_g1 == f_mul_g
+# now, the other way around, using ntt and intt instead of ntt_mert and intt_mert
+T = NTT(q)
+assert Mert.ntt_mert(Mert.prec(f)) == T.ntt(f)
+assert Mert.post(Mert.intt_mert(F)) == T.intt(F)
+assert Mert.ntt_mert(f) == T.ntt(Mert.post(f))
+assert Mert.intt_mert(F) == Mert.prec(T.intt(F))
+
+# now we compute the multiplication mod x^n-1 using T.(i)ntt:
+F1 = T.ntt(Mert.post(f))
+G1 = T.ntt(Mert.post(g))
+F1_mul_G1 = [(x*y) % q for (x, y) in zip(F1, G1)]
+f1_mul_g1 = Mert.prec(T.intt(F1_mul_G1))
+assert f1_mul_g1 == f_mul_g
