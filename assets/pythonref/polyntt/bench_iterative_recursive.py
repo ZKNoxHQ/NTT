507a1068072fa861db0b8c6dd21a3cc1ca6c5065
Simon Masson <simon.masson@yahoo.fr>
2025-02-11 17:43:39 +0100

    fix bench commands

--- assets/pythonref/polyntt/bench_iterative_recursive.py
+++ assets/pythonref/polyntt/bench_iterative_recursive.py
@@ -3,30 +3,38 @@ from polyntt.generate_test_vectors import deterministic_poly
 from polyntt.poly import Poly
 from polyntt.test_cases import TEST_CASES
 
-iterations = 100
-print("iterations:{}\n".format(iterations))
 
-print("q\tn\tIterative\t\tRecursive")
-for (q, two_adicity) in [TEST_CASES[1]]:
+class BenchIterativeRecursive:
+    def bench_iterative_recursive():
+        iterations = 100
+        print("iterations:{}\n".format(iterations))
 
-    for n in [1 << (two_adicity-2), 1 << (two_adicity-1)]:  # for two sizes of polynomials
-        print("{}\t{}".format(q, n), end='\t')
-        p1 = Poly(deterministic_poly(q, n), q)
-        p2 = Poly(deterministic_poly(q, n), q)
-        t1 = time()
-        for i in range(iterations):
-            p3 = p1*p2
-        t2 = time()
-        p3_iterative = p3
-        print("{:.2f} μs".format((t2-t1) * 10**6/iterations), end='\t\t')
+        print("q\tn\tIterative\t\tRecursive")
+        for (q, two_adicity) in [TEST_CASES[1]]:
 
-        p1 = Poly(deterministic_poly(q, n), q, 'NTTRecursive')
-        p2 = Poly(deterministic_poly(q, n), q, 'NTTRecursive')
-        t3 = time()
-        for i in range(iterations):
-            p3 = p1*p2
-        t4 = time()
-        p3_recursive = p3
-        print("{:.2f} μs".format((t4-t3) * 10**6/iterations))
+            # for two sizes of polynomials
+            for n in [1 << (two_adicity-2), 1 << (two_adicity-1)]:
+                print("{}\t{}".format(q, n), end='\t')
+                p1 = Poly(deterministic_poly(q, n), q)
+                p2 = Poly(deterministic_poly(q, n), q)
+                t1 = time()
+                for i in range(iterations):
+                    p3 = p1*p2
+                t2 = time()
+                p3_iterative = p3
+                print("{:.2f} μs".format(
+                    (t2-t1) * 10**6/iterations), end='\t\t')
 
-        assert p3_iterative == p3_recursive
+                p1 = Poly(deterministic_poly(q, n), q, 'NTTRecursive')
+                p2 = Poly(deterministic_poly(q, n), q, 'NTTRecursive')
+                t3 = time()
+                for i in range(iterations):
+                    p3 = p1*p2
+                t4 = time()
+                p3_recursive = p3
+                print("{:.2f} μs".format((t4-t3) * 10**6/iterations))
+
+                assert p3_iterative == p3_recursive
+
+
+BenchIterativeRecursive.bench_iterative_recursive()
--- assets/pythonref/polyntt/bench_poly_mul.py
+++ assets/pythonref/polyntt/bench_poly_mul.py
@@ -4,40 +4,48 @@ from polyntt.generate_test_vectors import deterministic_poly
 from polyntt.poly import Poly
 from polyntt.test_cases import TEST_CASES
 
-iterations = 100
-print("iterations:{}\n".format(iterations))
 
-print("        q\tn\tPatrick\t\tSchoolbook\tMert")
-for (q, two_adicity) in TEST_CASES:
+class BenchPolyMul:
 
-    for n in [1 << (two_adicity-2), 1 << (two_adicity-1)]:  # for two sizes of polynomials
-        print("{:10}\t{}".format(q, n), end='\t')
-        p1 = Poly(deterministic_poly(q, n), q)
-        p2 = Poly(deterministic_poly(q, n), q)
-        t1 = time()
-        for i in range(iterations):
-            p3 = p1*p2
-        t2 = time()
-        p3_patrick = p3
-        print("{:.2f} μs".format((t2-t1) * 10**6/iterations), end='\t')
+    def bench_poly_mul():
+        iterations = 100
+        print("iterations:{}\n".format(iterations))
 
-        p1 = Poly(deterministic_poly(q, n), q)
-        p2 = Poly(deterministic_poly(q, n), q)
-        t3 = time()
-        for i in range(iterations):
-            p3 = p1.mul_schoolbook(p2)
-        t4 = time()
-        p3_schoolbook = p3
-        print("{:.2f} μs".format((t4-t3) * 10**6/iterations), end='\t')
+        print("        q\tn\tPatrick\t\tSchoolbook\tMert")
+        for (q, two_adicity) in TEST_CASES:
 
-        Mert = NTTMert(q)
-        p1 = Poly(deterministic_poly(q, n), q)
-        p2 = Poly(deterministic_poly(q, n), q)
-        t5 = time()
-        for i in range(iterations):
-            p3 = Mert.poly_mul_mert(p1.coeffs, p2.coeffs)
-        t6 = time()
-        p3_mert = p3
-        print("{:.2f} μs".format((t6-t5) * 10**6/iterations))
+            # for two sizes of polynomials
+            for n in [1 << (two_adicity-2), 1 << (two_adicity-1)]:
+                print("{:10}\t{}".format(q, n), end='\t')
+                p1 = Poly(deterministic_poly(q, n), q)
+                p2 = Poly(deterministic_poly(q, n), q)
+                t1 = time()
+                for i in range(iterations):
+                    p3 = p1*p2
+                t2 = time()
+                p3_patrick = p3
+                print("{:.2f} μs".format((t2-t1) * 10**6/iterations), end='\t')
 
-        assert p3_mert == p3_patrick.coeffs and p3_mert == p3_schoolbook.coeffs
+                p1 = Poly(deterministic_poly(q, n), q)
+                p2 = Poly(deterministic_poly(q, n), q)
+                t3 = time()
+                for i in range(iterations):
+                    p3 = p1.mul_schoolbook(p2)
+                t4 = time()
+                p3_schoolbook = p3
+                print("{:.2f} μs".format((t4-t3) * 10**6/iterations), end='\t')
+
+                Mert = NTTMert(q)
+                p1 = Poly(deterministic_poly(q, n), q)
+                p2 = Poly(deterministic_poly(q, n), q)
+                t5 = time()
+                for i in range(iterations):
+                    p3 = Mert.poly_mul_mert(p1.coeffs, p2.coeffs)
+                t6 = time()
+                p3_mert = p3
+                print("{:.2f} μs".format((t6-t5) * 10**6/iterations))
+
+                assert p3_mert == p3_patrick.coeffs and p3_mert == p3_schoolbook.coeffs
+
+
+BenchPolyMul.bench_poly_mul()
